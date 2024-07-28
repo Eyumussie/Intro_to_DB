af0d23140fe1c6795da252a0c6002fec3ed5fb66
Eyumussie <87204461+Eyumussie@users.noreply.github.com>
2024-07-28 22:22:56 +0300

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- task_5.sql
+
+-- Use the passed database
+USE alx_book_store;
+
+-- Insert a single row into the customers table
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
